4be2dd70e071f3052d6ebe06aae3c1443cab2856
KhahnHax <71590195+khanhkenly54264@users.noreply.github.com>
2024-11-19 08:47:53 +0700

    T3-19/11/2024

--- Lesson38/Lesson03/Lesson03/obj/Debug/net8.0/Lesson03.AssemblyInfo.cs
+++ Lesson38/Lesson03/Lesson03/obj/Debug/net8.0/Lesson03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1922cf76c1ad36435ebe4c72569d42dde1d93042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f572900b09714c9499b492c6a9b829fa3377c47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
