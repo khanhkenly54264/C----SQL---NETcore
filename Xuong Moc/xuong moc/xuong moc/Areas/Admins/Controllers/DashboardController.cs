936a5add16047263716b62bfa1993f2884975df0
KhahnHax <71590195+khanhkenly54264@users.noreply.github.com>
2024-12-09 15:41:29 +0700

    T2-9/12/2024

---
+++ Xuong Moc/xuong moc/xuong moc/Areas/Admins/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xuong_moc.Areas.Admins.Controllers
+{
+    [Area("Admins")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
